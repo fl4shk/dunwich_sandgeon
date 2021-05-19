7189b046f97f5dde962322b6666996a4b1307c48
fl4shk <fl4shk@users.noreply.github.com>
2021-05-19 07:22:34 -0500

    add license notice to "misc_includes.hpp"

--- src/misc_includes.hpp
+++ src/misc_includes.hpp
@@ -3,6 +3,21 @@
 
 // src/misc_includes.hpp
 
+// This file is part of Dungwich Sandeon.
+// 
+// Dungwich Sandeon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+// 
+// Dungwich Sandeon is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along
+// with Dungwich Sandeon.  If not, see <https://www.gnu.org/licenses/>.
+
 #include "liborangepower_src/misc/misc_includes.hpp"
 #include "liborangepower_src/misc/misc_output_classes.hpp"
 #include "liborangepower_src/misc/misc_types.hpp"
