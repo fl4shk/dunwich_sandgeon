7c7dd96af83c3eacd85b30c294c0178aa6738035
fl4shk <fl4shk@users.noreply.github.com>
2021-06-01 07:43:11 -0500

    `src/menu_classes.hpp`: move `MenuNode` to `Menu::Node`

--- src/menu_classes.hpp
+++ src/menu_classes.hpp
@@ -23,62 +23,72 @@
 namespace dungwich_sandeon
 {
 
-struct MenuNode final
-{
-public:		// types
-	enum class Kind
-	{
-		// End of node list
-		End,
 
-		// Button triggers the function pointer
-		ActionButton,
-
-		// Buttone triggers the function pointer, which passes in value
-		ActionButtonParam,
-
-		// When picked, set the dialog exit type to `variable`
-		ExitButton,
-
-		// Pick between multiple options, from zero to max defined in
-		// `variable` (so use the highested allowed value +1)
-		HorizPicker,
-
-		// Pick between multiple options, but also wrap
-		HorizPickerWrap,
-	};
-	// Value to modify for horiz pickers
-	using DataValue = int*;
-
-	// Button action
-	using DataActionFunc = std::function<void()>;
-
-	// Button action with parameter
-	using DataActionParamFunc = std::function<void(int)>;
-
-	using VariantData = std::variant<std::monostate, DataValue,
-		DataActionFunc, DataActionParamFunc>;
-public:		// variables
-	Kind kind;
-
-	u32 flags;
-
-	// Bounding box for the object, for mouse/touch
-	sdl::Rect position;
-
-	// Where to go, using keyboard or controller; "" if nowhere
-	std::string left, right, up, down;
-
-	VariantData data;
-
-	// Various uses
-	int variable;
-
-	// When it's updated
-	std::function<void()> on_update;
-};
 class Menu final
 {
+public:		// types
+	struct Node final
+	{
+	public:		// types
+		enum class Kind
+		{
+			// End of node list
+			End,
+
+			// Button triggers the function pointer
+			ActionButton,
+
+			// Buttone triggers the function pointer, which passes in value
+			ActionButtonParam,
+
+			// When picked, set the dialog exit type to `variable`
+			ExitButton,
+
+			// Pick between multiple options, from zero to max defined in
+			// `variable` (so use the highested allowed value +1)
+			HorizPicker,
+
+			// Pick between multiple options, but also wrap
+			HorizPickerWrap,
+		};
+		// Value to modify for horiz pickers
+		using DataValue = int*;
+
+		// Button action
+		using DataActionFunc = std::function<void()>;
+
+		// Button action with parameter
+		using DataActionParamFunc = std::function<void(int)>;
+
+		using VariantData = std::variant<std::monostate, DataValue,
+			DataActionFunc, DataActionParamFunc>;
+
+		using OnUpdateFunc = std::function<void()>;
+	public:		// variables
+		// Who owns us?
+		Menu* parent;
+
+		// Index into `parent->_node_map`
+		std::string self_str;
+
+		Kind kind;
+
+		u32 flags;
+
+		// Where to go, using keyboard or controller; "" if nowhere
+		std::string left, right, up, down;
+
+		VariantData data;
+
+		// Various uses
+		int variable;
+
+		// When it's updated
+		OnUpdateFunc on_update;
+	};
+private:		// variables
+	std::map<std::string, Node> _node_map;
+public:		// functions
 };
 
 } // namespace dungwich_sandeon
--- src/sdl/real_main_sdl_class.hpp
+++ src/sdl/real_main_sdl_class.hpp
@@ -48,7 +48,6 @@ public:		// functions
 	~RealMainSdl() = default;
 
 	int run();
-
 private:		// functions
 };
 
